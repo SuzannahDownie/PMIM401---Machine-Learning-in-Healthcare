029c6daac01c535bfa0c6dfafc5593894e9a3337
SuzannahTTA <64838966+SuzannahDownie@users.noreply.github.com>
2022-02-22 15:27:43 +0000

    adding changes 22/02/2022

--- PMIM402 Assignment/KMeans_Clustering_2131572.R
+++ PMIM402 Assignment/KMeans_Clustering_2131572.R
@@ -6,7 +6,21 @@ heart <- read.csv("heart-c.csv")
 
 # create new dataframe with only continuous variables for kmeans
 heart <- heart %>% select(c(age, trestbps, chol, thalach, oldpeak))
-heart_norm <- scale(heart) %>% as.data.frame
-View(heart_norm)
 
-# adding a comment to make a change
+# create histograms
+heart %>% gather(attributes, value, 1:ncol(heart)) %>%
+  ggplot(aes(x=value)) +
+  geom_histogram(fill="lightblue", color="black") +
+  facet_wrap(~attributes, scales = 'free_x') +
+  theme_bw()
+
+heart_norm <- scale(heart) %>% as.data.frame
+
+k_means <- kmeans(heart_norm, centers=3)
+
+sse <- vector()
+for (i in 1:10) {
+  sse[i] <- kmeans(heart, i)$tot.withinss
+}
+
+plot(sse, type= "o")
