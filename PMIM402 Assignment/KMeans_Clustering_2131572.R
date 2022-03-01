c0b405995e8eb7ec90d01ef2e51ee6da15766571
SuzannahTTA <64838966+SuzannahDownie@users.noreply.github.com>
2022-03-01 15:48:03 +0000

    Kmeans updated

--- PMIM402 Assignment/KMeans_Clustering_2131572.R
+++ PMIM402 Assignment/KMeans_Clustering_2131572.R
@@ -1,42 +1,68 @@
 library(tidyverse)
 library(factoextra)
-options(repr.plot.width=15,repr.plot.height=10)
+
 # read in the dataset
 heart <- read.csv("heart-c.csv")
+# create new dataframe with only continuous variables for kmeans and remove
+# the index
+heart <- heart %>% select(age, trestbps, chol, 
+                          thalach, oldpeak)
 
-# create new dataframe with only continuous variables for kmeans
-heart <- heart %>% select(c(age, trestbps, chol, thalach, oldpeak))
+# create histograms of continuous data
+heart %>% gather(attribute, value, 1:ncol(heart)) %>%
+  ggplot(aes(x = value)) + 
+  geom_histogram(fill = "salmon1", color = "black") +
+  facet_wrap(~attribute, scales = 'free') +
+  theme_grey()
 
-# create histograms
-heart %>% gather(attributes, value, 1:ncol(heart)) %>%
-  ggplot(aes(x=value)) +
-  geom_histogram(fill="lightblue", color="black") +
-  facet_wrap(~attributes, scales = 'free_x') +
-  theme_bw()
-
+# normalise the data so as it sits within a similar range
 heart_norm <- scale(heart) %>% as.data.frame
 
-k_means <- kmeans(heart_norm, centers=2)
+# visualise the sum of squared error to determine the initial
+# optimum no. of clusters
+fviz_nbclust(x = heart_norm, FUNcluster = kmeans, method = "wss") +
+  geom_vline(xintercept = 2, linetype = 2) +
+  geom_vline(xintercept = 5, linetype = 2) 
 
-sse <- vector()
-for (i in 1:10) {
-  sse[i] <- kmeans(heart, i)$tot.withinss
-}
+# visualise the silhouette scores to support elbow plot above
+fviz_nbclust(x = heart_norm, FUNcluster = kmeans, method = 'silhouette')
 
-plot(sse, type= "o")
+# run kmeans with 2 clusters and with 5
+kmeans_2 <- kmeans(heart_norm, centers = 2)
+kmeans_5 <- kmeans(heart_norm, centers = 5)
 
-heart_cluster <- cbind(heart_norm, "Cluster" = k_means$cluster)
-heart_cluster$Cluster <- as.factor(heart_cluster$Cluster)
+# visualise both models in 2D
+fviz_cluster(kmeans_2, geom = "point", pointsize = 2, data = heart_norm, main = 'Cluster Plot in 2D Space - 2 Clusters', 
+             xlab = 'Dimension 1', ylab = 'Dimension 2')
+fviz_cluster(kmeans_5, geom = "point", pointsize = 2, data = heart_norm, main = 'Cluster Plot in 2D Space - 5 Clusters', 
+             xlab = 'Dimension 1', ylab = 'Dimension 2')
 
-heart_cluster %>% gather(attributes, value, 1:ncol(heart_cluster)-1) %>%
-  ggplot(aes(x=value, group = Cluster, fill = Cluster)) +
-  geom_histogram(alpha=0.6, bins = 20, color = "black") +
-  facet_wrap(~attributes, scales = 'free_x') +
-  theme_bw()
+# add cluster values to dataframes as factors
+# Two Clusters
+heart_clust_2 <- cbind(heart, 'Cluster' = kmeans_2$cluster)
+heart_clust_2$Cluster <- as.factor(heart_clust_2$Cluster)
+
+# Five Clusters
+heart_clust_5 <- cbind(heart, 'Cluster' = kmeans_5$cluster)
+heart_clust_5$Cluster <- as.factor(heart_clust_5$Cluster)
+
+# plot histograms to show distribution across the clusters
+heart_clust_2 %>% gather(attribute, value, 1:ncol(heart_clust_2)-1) %>%
+  ggplot(aes(x = value, group = Cluster, fill = Cluster)) + 
+  geom_histogram(alpha = 0.6, bins = 30, color = 'black') +
+  facet_wrap(~attribute, scales = 'free') +
+  theme_grey()
+
+heart_clust_5 %>% gather(attribute, value, 1:ncol(heart_clust_5)-1) %>%
+  ggplot(aes(x = value, group = Cluster, fill = Cluster)) + 
+  geom_histogram(alpha = 0.6, bins = 30, color = 'black') +
+  facet_wrap(~attribute, scales = 'free') +
+  theme_grey()
 
 
-fviz_cluster(k_means, geom = "point", data=heart_norm)
 
-dist_mat <- dist(heart_cluster, method = "euclidean")
-hir_clust <- hclust(dist_mat, method = 'average')
-plot(hir_clust)
+
+
+# dist_mat <- dist(heart_cluster, method = "euclidean")
+# hir_clust <- hclust(dist_mat, method = 'average')
+# plot(hir_clust)
